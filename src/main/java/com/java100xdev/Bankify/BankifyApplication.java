236638943ad3f63b5af4216822f6ff406d6151dc
SridharVadla <vadlasreedhar123@gmail.com>
2024-06-08 14:45:54 +0530

    Intialized an spring Boot Backend Back Project

---
+++ src/main/java/com/java100xdev/Bankify/BankifyApplication.java
@@ -0,0 +1,13 @@
+package com.java100xdev.Bankify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java100xdev/Bankify/BankifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java100xdev.Bankify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
